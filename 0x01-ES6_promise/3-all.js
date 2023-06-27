2565a5385b13b06147d05ab365145110a0228c52
okoroafor wisdom onyinye <104413525+wisdomoo@users.noreply.github.com>
2023-06-27 22:06:20 +0100

    Files uploaded

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,4 +7,4 @@ export default function handleProfileSignup() {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
-}￼Enter
+}
