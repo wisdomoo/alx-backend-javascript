bf58b2a1ccf9cbd56d6b01e47e585a69419c0dd5
wisdom <okoroaforwisdomo@gmail.com>
2023-07-03 10:05:29 -0400

    files uploaded

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * This function retrieves a list of students.
+ * @author Monday Odoh <https://github.com/Monemax94>
+ * @returns {{id: Number, firstName: String, Location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
